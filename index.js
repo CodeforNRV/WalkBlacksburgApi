c7a08cd7662136505d0fbb7ded956e717e2a8f21
Neal Feierabend <neal@nealf.com>
2015-09-15 11:19:14 -0400

    Added /crime end point to get crime data

--- index.js
+++ index.js
@@ -70,6 +70,54 @@ dispatcher.onGet("/inBlacksburg", function(req, res) {
     });
 });
 
+dispatcher.onGet("/crime", function(req, res) {
+    var enddate = new Date(); //Default end date is today
+    var startdate = new Date();
+    startdate.setMonth(startdate.getMonth() - 6) //Default start date 6 months prior to today
+    if (typeof req.params.startdate != 'undefined') {
+        startdate = new Date(req.params.startdate); //Format should be YYYY-MM-DD
+    }
+    if (typeof req.params.enddate != 'undefined') {
+        enddate = new Date(req.params.enddate);
+    }
+    // get a pg client from the connection pool
+    pg.connect(conString, function(err, client, done) {
+        var handleError = function(err) {
+            // no error occurred, continue with the request
+            if(!err) return false;
+            // An error occurred, remove the client from the connection pool.
+            // A truthy value passed to done will remove the connection from the pool
+            // instead of simply returning it to be reused.
+            // In this case, if we have successfully received a client (truthy)
+            // then it will be removed from the pool.
+            if(client){
+                done(client);
+            }
+            console.log(err);
+            res.writeHead(500, {'content-type': 'application/json','Access-Control-Allow-Origin' : '*'});
+            res.end(JSON.stringify({"error":"There was an error querying the database"}));
+            return true;
+        };
+        // handle an error from the connection
+        if(handleError(err)) return;
+        var sql = "SELECT row_to_json(featcoll) FROM " +
+            "(SELECT 'FeatureCollection' As type, array_to_json(array_agg(feat)) As features FROM " +
+                "(SELECT 'Feature' As type, ST_AsGeoJSON(ST_Transform(ST_Simplify(geom, 2),4326),5)::json As geometry," +
+                    "row_to_json((SELECT l FROM (SELECT agencyname, crimecode, datereported, description, location) As l)) As properties " +
+                    "FROM crime As tbl WHERE datereported BETWEEN $1 AND $2)" +
+                "AS feat)  " +
+            "AS featcoll;";
+        client.query(sql, [startdate, enddate], function(err, result) {
+            // handle an error from the query
+            if(handleError(err)) return;
+            // return the client to the connection pool for other requests to reuse
+            done();
+            res.writeHead(200, {'Content-Type': 'application/json','Access-Control-Allow-Origin' : '*'});
+            res.end(JSON.stringify(result.rows[0].row_to_json));
+        });
+    });
+});
+
 //A POST request
 dispatcher.onPost("/score", function(req, res) {
     var coordinates = req.body;
@@ -118,11 +166,12 @@ dispatcher.onPost("/score", function(req, res) {
     });
 });
 
-dispatcher.onGet("/updateCrime", function(req, res) {
+dispatcher.onPost("/updateCrime", function(req, res) {
     //Get the crime data
-    var start = req.params.start; //eg 08/01/2015
-    var end = req.params.end;
-    console.log(start);
+    var startdate = req.body.startdate; //eg 08/01/2015
+    var enddate = req.body.enddate;
+    var pw = req.body.passcode;
+    console.log(startdate);
 
     var http = require("http");
 
@@ -130,7 +179,7 @@ dispatcher.onGet("/updateCrime", function(req, res) {
         "method": "GET",
         "hostname": "www.crimemapping.com",
         "port": null,
-        "path": "/GetIncidents.aspx?db=" + start + "&de=" + end + "&ccs=AR%2CAS%2CBU%2CDP%2CDR%2CDU%2CFR%2CHO%2CVT%2CRO%2CSX%2CTH%2CVA%2CVB%2CWE&xmin=-8954791.68972816&ymin=4471101.623740454&xmax=-8948265.878438141&ymax=4472267.288421833",
+        "path": "/GetIncidents.aspx?db=" + startdate + "&de=" + enddate + "&ccs=AR%2CAS%2CBU%2CDP%2CDR%2CDU%2CFR%2CHO%2CVT%2CRO%2CSX%2CTH%2CVA%2CVB%2CWE&xmin=-8954791.68972816&ymin=4471101.623740454&xmax=-8948265.878438141&ymax=4472267.288421833",
         "headers": {}
     };
 
@@ -151,7 +200,7 @@ dispatcher.onGet("/updateCrime", function(req, res) {
                     "VALUES ('"+crime["AgencyID"]+"', '"+crime["AgencyName"]+"', '"+crime["CaseNumber"]+"', '"+crime["CrimeCodeID"]+"', '"+crime["CrimeCode"]+"', '" + crime["DateReported"] +
                     "', '"+crime["Description"]+"', '"+crime["Location"]+"', "+crime["ObjectID"]+", ST_GeomFromText('POINT(" + crime["Y"] + " " + crime["X"] + ")',3395));";
             }
-            pg.connect("postgres://" + process.env.PG_USERNAME + ":" + process.env.PG_PASSWORD + "@postgres1.ceipocejvkue.us-west-2.rds.amazonaws.com/blacksburg", function(err, client, done) {
+            pg.connect("postgres://crimeuser:" + pw + "@postgres1.ceipocejvkue.us-west-2.rds.amazonaws.com/blacksburg", function(err, client, done) {
                 var handleError = function(err) {
                     // no error occurred, continue with the request
                     if(!err) return false;
@@ -176,7 +225,6 @@ dispatcher.onGet("/updateCrime", function(req, res) {
 
                 });
             });
-
 
         });
     });
